f8d0b463e4500afe0b5a5021ed5d0e7e13ebc08a
Vals098 <valeria.farinosi@gmail.com>
2026-05-06 17:03:08 +0200

    fix: MovieDetails function

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router"
 
-const {movieId} = useParams()
-
-const MovieDetails = function(){
-    return(){
-        <h1>Movie Details</h1>\
-        <p>ID film: {movieId}</p>
-    }
+const MovieDetails = function () {
+  const { movieId } = useParams()
+  return (
+    <>
+      <h1>Movie Details</h1>
+      <p>ID film: {movieId}</p>
+    </>
+  )
 }
 
 export default MovieDetails
